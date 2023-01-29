c2f5c2aa4bb1ec4e6d495f365088ca9b7a44b753
Panachai <panachai.ny@gmail.com>
2023-01-29 23:35:06 +0700

    Update example-table.sql

--- example-table.sql
+++ example-table.sql
@@ -7,3 +7,32 @@ create table example.persons (
 	age int4,
 	constraint persons_pk primary key (id)
 );
+
+-- mock datas
+
+insert
+	into
+	example.persons ("name",
+	lastname,
+	age)
+values ('john',
+'marvel',
+33);
+
+insert
+	into
+	example.persons ("name",
+	lastname,
+	age)
+values ('john',
+'dc',
+34);
+
+insert
+	into
+	example.persons ("name",
+	lastname,
+	age)
+values ('bell',
+'marvel',
+24);
