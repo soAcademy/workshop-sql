e3729e7a5fb9a0409503a4732354af27534ac040
Panachai <panachai.ny@gmail.com>
2023-01-29 23:48:53 +0700

    Update example-table.sql

--- example-table.sql
+++ example-table.sql
@@ -1,6 +1,6 @@
-create schema example;
+create schema sprint_tech_bootcamp;
 
-create table example.persons (
+create table sprint_tech_bootcamp.persons (
 	id int4 not null generated always as identity,
 	"name" varchar null,
 	lastname varchar null,
@@ -12,27 +12,82 @@ create table example.persons (
 
 insert
 	into
-	example.persons ("name",
+	sprint_tech_bootcamp.persons
+( "name",
 	lastname,
 	age)
-values ('john',
+values('john',
 'marvel',
 33);
-
 insert
 	into
-	example.persons ("name",
+	sprint_tech_bootcamp.persons
+( "name",
 	lastname,
 	age)
-values ('john',
+values('john',
 'dc',
 34);
-
 insert
 	into
-	example.persons ("name",
+	sprint_tech_bootcamp.persons
+( "name",
 	lastname,
 	age)
-values ('bell',
+values('bell',
 'marvel',
 24);
+insert
+	into
+	sprint_tech_bootcamp.persons
+( "name",
+	lastname,
+	age)
+values('yuki',
+'marvel',
+24);
+insert
+	into
+	sprint_tech_bootcamp.persons
+( "name",
+	lastname,
+	age)
+values( 'hiro',
+'marvel',
+22);
+insert
+	into
+	sprint_tech_bootcamp.persons
+( "name",
+	lastname,
+	age)
+values( 'jame',
+'dc',
+33);
+insert
+	into
+	sprint_tech_bootcamp.persons
+( "name",
+	lastname,
+	age)
+values( 'jo',
+'dc',
+22);
+insert
+	into
+	sprint_tech_bootcamp.persons
+( "name",
+	lastname,
+	age)
+values( 'suke',
+'marvel',
+5);
+insert
+	into
+	sprint_tech_bootcamp.persons
+( "name",
+	lastname,
+	age)
+values( 'kaiju',
+'marvel',
+13);
